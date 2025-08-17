22e94425c754303c4cc315e0fb1ec6961137186e
esther <ogbonnaesther52@gmail.com>
2025-08-17 16:51:36 +0100

    add script

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,4 @@
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'Books' AND TABLE_SCHEMA = DATABASE();
+select column_name, column_type, is_nullable, column_key, extra
+from information_schema.columns
+where table_name = 'books' and table_schema = alx_book_store;
+
